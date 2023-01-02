fb70009ceed7ff43392c83171198634dd9582bbe
junseojjang <parkjunseo0623@naver.com>
2023-01-02 23:25:07 +0900

    Reset 들으면서 했어요 (/playlist 추가)

---
+++ playlist/index.js
@@ -0,0 +1,23 @@
+function selectbar() {
+  const selectList = document.getElementById("selectMenu");
+  if (
+    selectList.style.visibility == "hidden" ||
+    selectList.style.visibility == ""
+  ) {
+    selectList.style.visibility = "visible";
+    selectList.style.opacity = 1;
+  } else {
+    selectList.style.visibility = "hidden";
+    selectList.style.opacity = 0;
+  }
+}
+
+let typeTranstoDiv = {
+  SeeMore: ".main > .AboutMe",
+};
+
+function scrollF(type) {
+  const DivRoot = typeTranstoDiv[type];
+  const aboutQuery = document.querySelector(DivRoot);
+  window.scrollTo({ top: aboutQuery.offsetTop - 50, behavior: "smooth" });
+}
